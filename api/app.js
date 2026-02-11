66cd14f789b672d9412ba77260335374b9523768
FacuMoris <89667302+FacuMoris@users.noreply.github.com>
2026-02-10 23:15:42 -0300

    Inicio API backend

---
+++ api/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const connection = require("./db");
+
+const app = express();
+const port = 8888;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log("Servidor iniciado en: http://localhost:" + port);
+});
+
+app.get("/test", async (req, res) => {
+  const query = `
+  SELECT id, author_user_id, title, description
+  FROM recipe
+  `;
+
+  try {
+    [results] = await connection.query(query);
+    res.json({ success: true, results });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ success: false, message: "Error" });
+  }
+});
+
+app.use((req, res, next) => {
+  res.status(404);
+  res.send(`
+        <h1>404 Not Found</h1>
+        `);
+});
