e62f2d90f3c49c7036849ccd83e2ea8e3f486c6e
FacuMoris <89667302+FacuMoris@users.noreply.github.com>
2026-02-12 00:44:38 -0300

    CRUD inicial Recipe

--- api/src/controllers/recipeController.js
+++ api/src/controllers/recipeController.js
@@ -1,17 +1,5 @@
 const recipeModel = require("../models/recipeModel");
 
-async function getAllRecipes(req, res, next) {
-  try {
-    const recipes = await recipeModel.getAll();
-    res.json({
-      success: true,
-      data: recipes,
-    });
-  } catch (err) {
-    next(err);
-  }
-}
-
 async function createRecipe(req, res, next) {
   try {
     const { author_user_id, title, description } = req.body;
@@ -32,6 +20,18 @@ async function createRecipe(req, res, next) {
       success: true,
       message: "Recipe created",
       transaction_id: id,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function getAllRecipes(req, res, next) {
+  try {
+    const recipes = await recipeModel.getAll();
+    res.json({
+      success: true,
+      data: recipes,
     });
   } catch (err) {
     next(err);
@@ -59,8 +59,60 @@ async function getRecipeById(req, res, next) {
   }
 }
 
+async function updateRecipe(req, res, next) {
+  try {
+    const { id } = req.params;
+    const { title, description } = req.body;
+
+    if (!title && !description) {
+      return res.status(400).json({
+        success: false,
+        message: "info required to update",
+      });
+    }
+    const affected = await recipeModel.updateById(id, { title, description });
+
+    if (!affected) {
+      return res.status(404).json({
+        success: false,
+        message: "Recipe not found",
+      });
+    }
+    const updated = await recipeModel.getById(id);
+    return res.json({
+      success: true,
+      message: "Recipe updated",
+      data: updated,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function deleteRecipe(req, res, next) {
+  try {
+    const { id } = req.params;
+    const affected = await recipeModel.softDeleteById(id);
+
+    if (!affected) {
+      return res.status(404).json({
+        success: false,
+        message: "Recipe not found",
+      });
+    }
+    return res.json({
+      success: true,
+      message: "Recipe updated",
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
 module.exports = {
   getAllRecipes,
   getRecipeById,
   createRecipe,
+  updateRecipe,
+  deleteRecipe,
 };
--- api/src/models/recipeModel.js
+++ api/src/models/recipeModel.js
@@ -1,17 +1,5 @@
 const connection = require("../../db");
 
-async function getAll() {
-  {
-    const query = `
-        SELECT id, author_user_id, title, description
-        FROM recipe
-        WHERE deleted_at IS NULL`;
-
-    const [rows] = await connection.query(query);
-    return rows;
-  }
-}
-
 async function create(recipe) {
   const query = `
     INSERT INTO recipe (
@@ -32,6 +20,18 @@ async function create(recipe) {
   return result.insertId;
 }
 
+async function getAll() {
+  {
+    const query = `
+        SELECT id, author_user_id, title, description
+        FROM recipe
+        WHERE deleted_at IS NULL`;
+
+    const [rows] = await connection.query(query);
+    return rows;
+  }
+}
+
 async function getById(id) {
   const query = `
     SELECT id, author_user_id, title, description
@@ -43,8 +43,42 @@ async function getById(id) {
   return rows[0] || null;
 }
 
+async function updateById(id, data) {
+  const query = `
+    UPDATE recipe
+    SET title = ?,
+    description = ?,
+    updated_at = NOW()
+    WHERE id = ?
+    AND deleted_at IS NULL
+    `;
+
+  const [result] = await connection.query(query, [
+    data.title,
+    data.description,
+    id,
+  ]);
+
+  return result.affectedRows;
+}
+
+async function softDeleteById(id) {
+  const query = `
+    UPDATE recipe
+    SET updated_at = NOW(),
+    deleted_at = NOW()
+    WHERE id = ?
+    AND deleted_at IS NULL
+    `;
+
+  const [result] = await connection.query(query, [id]);
+  return result.affectedRows;
+}
+
 module.exports = {
   getAll,
   getById,
   create,
+  updateById,
+  softDeleteById,
 };
