c71645083c5407a78ed8c719f02fa4d8a0d47238
FacuMoris <89667302+FacuMoris@users.noreply.github.com>
2026-02-11 22:35:49 -0300

    Genero estructura /src

--- api/app.js
+++ api/app.js
@@ -5,6 +5,14 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/health", (req, res) => {
+  res.json({
+    status: "ok",
+    service: "recetarium-api",
+  });
+});
+
+// Luego borrar /test
 app.get("/test", async (req, res) => {
   const query = `
   SELECT id, author_user_id, title, description
@@ -12,19 +20,32 @@ app.get("/test", async (req, res) => {
   `;
 
   try {
-    [results] = await connection.query(query);
+    const [results] = await connection.query(query);
     res.json({ success: true, results });
   } catch (e) {
     console.log(e);
     res.status(500).json({ success: false, message: "Error" });
   }
 });
+// Luego borrar /error
+
+app.get("/error", (req, res, next) => {
+  next(new Error("Test 500"));
+});
 
 app.use((req, res, next) => {
-  res.status(404);
-  res.send(`
-        <h1>404 Not Found</h1>
-        `);
+  res.status(404).json({
+    success: false,
+    message: "404 Not Found",
+  });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({
+    success: false,
+    message: "Internal Server Error",
+  });
 });
 
 module.exports = app;
--- api/server.js
+++ api/server.js
@@ -2,5 +2,5 @@ const app = require("./app");
 const port = process.env.PORT || 8888;
 
 app.listen(port, () => {
-  console.log("API running on: http://" + process.env.DB_HOST + ":" + port);
+  console.log(`API running on: http://localhost:${port}`);
 });
